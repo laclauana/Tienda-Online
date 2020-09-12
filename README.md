a38dc018b93a533b70e882155ca09fc4a9a4a9dc
anaclette <laclauana@gmail.com>
2020-09-11 22:05:59 -0300

    improved style properties, added readme, still in progress

---
+++ README.md
@@ -0,0 +1,17 @@
+# Online shopping store
+
+## Second project for Ada ITW 
+
+This is the 2nd challenge that Ada Institute prepared for us: an online shopping store, this time implementing JavaScript programming language. I couldn't be more grateful for these last weeks, as I feel like my technical skills were taken to a whole different level, and it's certainly the most rewarding experience that 2020 isolation circumstances indulged me with to keep my spirit going!. If this is just a 2 months result, I can't dare to imagine what comes ahead in this training course.
+You might want to take a look at the online store model we were based on, in terms of structure, JS code and so forth: [https://frontend-proyecto-tienda.adaitw.org/].
+
+## *** Work in progress *** not quite ready! ##
+
+
+ <!-- ## Technologies used
+
+ Short HTML, a bit larger CSS and tons of Javascript.
+
+### Where to check on my online shopping store
+Hope you like it!
+[] -->
--- index.js
+++ index.js
@@ -1,3 +1,111 @@
 const filtroPagina = document.querySelector("#pagina")
 const tarjetas = document.getElementsByClassName("producto")
-const puntaje = 
+const puntaje = document.querySelector(".score")
+const filtroBusqueda = document.getElementsByClassName("score")
+
+
+
+// // cuando se escriba algo en el input
+// filtroNombre.oninput = () => {
+//     // recorro una a una cada tarjeta
+//     for (let tarjeta of tarjetas) {
+//         // me fijo el nombre de la tarjeta y qué buscó el usuario
+//         const titulo = tarjeta.dataset.nombre;
+//         const busqueda = filtroNombre.value;
+//         // si el titulo incluye lo que buscó el usuario...
+//         if (titulo.includes(busqueda)) {
+//             // le quito la clase "hidden" a la tarjeta
+//             tarjeta.classList.remove('hidden');
+//         } else {
+//             // se la agrego
+//             tarjeta.classList.add('hidden');
+//         }
+//     }
+// };
+
+// // recorro uno a uno los checkbox
+// for (let checkbox of filtroRating) {
+//     // si le hacen clic a uno de los checkbox
+//     checkbox.onclick = () => {
+//         // recorro una a una las tarjetas
+//         for (let tarjeta of tarjetas) {
+//             // si el checkbox esta seleccionado
+//             if (checkbox.checked) {
+//                 const rating = tarjeta.dataset.rating;
+//                 // me fijo si el valor del checkbox es igual al rating de la tarjeta
+//                 if (checkbox.value === rating) {
+//                     tarjeta.classList.remove('hidden');
+//                 } else {
+//                     tarjeta.classList.add('hidden');
+//                 }
+//                 // si el checkbox NO esta seleccionado... 
+//             } else {
+//                 tarjeta.classList.remove('hidden');
+//             }
+//         }
+//     };
+// }
+
+// const boton = document.querySelector('#boton')
+
+// const limpiarFiltro = () => {
+//     const listaDeCheckbox = document.getElementsByClassName('filter')
+//     filtroNombre.value = " "
+//     for (let checkbox of listaDeCheckbox) {
+//         checkbox.checked = false
+//     }
+// }
+
+// boton.onclick = () => {
+
+//     limpiarFiltro()
+// }
+
+
+
+
+
+// <button id="abrir-carrito">Carrito</button>
+
+// <div class="carrito">
+//   <h2>Carrito de compras</h2>
+//   <p>Tus productos son</p>
+//   <p>Celular Samsung</p>
+
+//   <label>Pagar con tarjeta
+//     <input type="checkbox" value="tarjeta" id="tarjeta">
+//   </label>
+//   <p id="recargo"></p>
+//   <p id="subtotal">0</p>
+//   <p id="total">0</p>
+
+//   <button id="cerrar-carrito">Salir</button>
+// </div>
+
+// const carrito = document.querySelector(".carrito")
+// const subtotal = document.querySelector("#subtotal")
+// const total = document.querySelector("#total")
+// const recargoParrafo = document.querySelector("#recargo")
+// const checkboxTarjeta = document.querySelector("#tarjeta")
+// const botonMostrarCarrito = document.querySelector("#abrir-carrito")
+// const botonOcultarCarrito = document.querySelector("#cerrar-carrito")
+
+// const subtotalProductos = 5999
+
+// carrito.classList.add("ocultar")
+
+// botonMostrarCarrito.onclick = () => {
+//   subtotal.textContent = subtotalProductos
+//  carrito.classList.remove("ocultar")
+// }
+
+// botonOcultarCarrito.onclick = () => {
+//   carrito.classList.add("ocultar")
+// }
+
+// checkboxTarjeta.onclick = () => {
+//   const recargo = subtotalProductos * 0.1
+//   recargoParrafo.textContent = recargo
+//   total.textContent = subtotalProductos + recargo
+// }
+
