9861c645a22c02922a14583d2dff2c9f5e395d8c
anaclette <laclauana@gmail.com>
2020-09-16 22:55:55 -0300

    worked on accessibility insights

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const limpiar = document.querySelector(".fa-trash-alt")
 const checkbox = document.querySelectorAll("input")
 const imagenesInstrumentos = document.querySelectorAll(".contenedor-imagen .modo-lista") // o ".vista-cuadricula"
 const botonComprar = document.querySelectorAll(".comprar")
-const carrito = document.querySelector(".fas.fa-shopping-cart")
-const cantidadCarrito = document.querySelector(".carrito > p")
+const carrito = document.querySelector(".carrito > button")
+const cantidadCarrito = document.querySelector(".carrito span")
 const carritoAbierto = document.querySelector(".contenedor-carrito-abierto")
 const overlay = document.querySelector(".overlay-contenido")
 const cerrarCarrito = document.querySelector(".fa.fa-times")
@@ -18,12 +18,12 @@ const cerrarCarrito = document.querySelector(".fa.fa-times")
 //  -------------- acciones boton carrito ---------------
 
 carrito.onclick = () => {
-    carritoAbierto.classList.remove("ocultar")
-    overlay.classList.remove("ocultar")
+    carritoAbierto.classList.toggle("ocultar")
+    overlay.classList.toggle("ocultar")
 }
 cerrarCarrito.onclick = () => {
-    carritoAbierto.classList.add("ocultar")
-    overlay.classList.add("ocultar")
+    carritoAbierto.classList.toggle("ocultar")
+    overlay.classList.toggle("ocultar")
 }
 
 for (let botones of botonComprar) {
@@ -33,7 +33,14 @@ for (let botones of botonComprar) {
     }
 }
 
+// filtroBusqueda.oninput = () => {
 
+// }
+
+
+// const borrarInputs = () => {
+
+// }
 
 
 // const producto = document.querySelectorAll(".producto")
@@ -194,4 +201,56 @@ window.onkeydown = (e) => {
 // const obtenerCalculoEnvio = (precio) => {
 //     let envio = obtenerGastoDeEnvio(precio) - precio
 //     return envio
+// }
+
+
+
+
+// input type submit es para cuando enviamos el formulario.
+// el atributo value es lo que dira el boton dentro.
+
+//input type text, type number, type email, type date, type color, radio, select. El button lleva type button
+//select es una etiqueta que lleva atributo name
+//dentro lleva etiquetas option con el value de la abreviacion de ese contenido por ej.
+
+// en mi codigo SIEMPRE HABRA VALUE en el INPUT que es lo que escribe el usuario.
+// excepto en edad y etc datos personales.
+// los que NO llevan value son input type CHECKBOX, RADIO y SELECT.
+// cuando les doy value esos tres no es para el usuario.
+
+// const fecha = document.querySelector("#fecha")  <-- input
+// console.log(fecha.value) <-- en ese caso vere la fecha que ingrese en el value
+
+// const inputEdad = document.querySelector("#input-edad")
+// form.onsubmit = (e) => {
+// e.preventDefault();
+// if (inputEdad.value < 18) {
+// checkEdad.textContent = "Sos menor de edad"
+// inputEdad.classList.add("input-error")
+// }
+// console.log(inputEdad.value)
+// }
+// si quiero que se envie la funcion es form.submit pero no se usa.
+
+// inputPassword.onblur = (e) => {     // cuando el usuario hace foco el evento es focus, cuando sale del input es blur. Solo se dispara onblur si previamente hubo onfocus
+    // if (inputPassword.value.length < 8) {
+        // checkPassword.textContent = "Tu password es muy corta"
+        // inputPassword.classList.add("input-error")
+//     }
+// }
+
+// evento de input .onchange alude a toda vez que cambie algo por parte del usuario. Escucha cualquier cambio que haya ocurrido en mi input.
+// el evento .oninput alude a cuando el usuario escribe en mi input
+
+
+// si uso por ejemplo
+// inputName.oninput = (e) => {
+    // console.log(e)
+// }                       voy a estar oyendo lo que puso el usuario en ese evento
+
+// const boton = document.querySelector("button")
+// boton.onclick = (e) => {   // puede ser e o event
+// la E trae informacion sobre el evento en si. 
+// si ingresamos console.log veremos por ejemplo el TARGET que nos indica el elemento al que le hicieron clic
+    // console.log("hiciste clic")
 // }
